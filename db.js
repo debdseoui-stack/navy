86d20f063f68f9dd457a0f237e89aa357aeb8e35
debdseoui-stack <debdseoui@gmail.com>
2026-01-07 09:07:42 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    //await mongoose.connect('mongodb+srv://dbuser:cLa9fUpMDXDgziS5@cluster0.s4o1w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    await mongoose.connect('mongodb://127.0.0.1:27017/nextappdb', { 
+    await mongoose.connect('mongodb+srv://no3sqnbaf:FWWkPxii3qJ6xcSs@cluster0.tlttord.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    //await mongoose.connect('mongodb://127.0.0.1:27017/nextappdb', { 
      
       useNewUrlParser: true,
       useUnifiedTopology: true,
